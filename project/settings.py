28b2e53252636504a02693dc90123947a5190851
somoire <rayr30667@gmail.com>
2019-05-25 15:19:56 -0400

    created the database .

--- project/settings.py
+++ project/settings.py
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'rodney',
+    'PASSWORD':'12345',
     }
 }
 
