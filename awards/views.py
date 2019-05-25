1066f71ae97b09415f13b80a26991b58daa15ee5
somoire <rayr30667@gmail.com>
2019-05-25 13:39:44 -0400

    search view created

--- awards/views.py
+++ awards/views.py
@@ -154,3 +154,20 @@ def site(request,site_id):
         form = RatingForm()
 
     return render(request,"site.html",{"project":project,"profile":profile,"ratings":ratings,"form":form})
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    current_user = request.user
+    profile =Profile.objects.get(username=current_user)
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Project.search_project(search_term)
+        message=f"{search_term}"
+
+        print(searched_projects)
+
+        return render(request,'search.html',{"message":message,"projects":searched_projects,"profile":profile})
+
+    else:
+        message="You haven't searched for any term"
+        return render(request,'search.html',{"message":message})
