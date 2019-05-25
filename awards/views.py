f041b5463e737b5a8f960d4c3f62751471a6554f
somoire <rayr30667@gmail.com>
2019-05-25 13:33:44 -0400

    index view created

--- awards/views.py
+++ awards/views.py
@@ -18,3 +18,22 @@ from .serializer import ProfileSerializer,ProjectSerializer,technologiesSerializ
 
 
 # Create your views here.
+def index(request):
+    date = dt.date.today()
+    winners=Project.objects.all()[:4]
+    caraousel = Project.objects.order_by('-overall_score')[0]
+    nominees=Project.objects.all()[4:8]
+    directories=Project.objects.all()[8:11]
+    resources=Project.objects.all()[11:15]
+    resources2=Project.objects.all()[15:19]
+
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile =Profile.objects.get(username=current_user)
+        print(current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request,'index.html',{"winners":winners,"profile":profile,"caraousel":caraousel,"date":date,"nominees":nominees,"directories":directories,"resources":resources,"resources2":resources2})
