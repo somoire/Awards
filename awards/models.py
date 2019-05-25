f00bbbc83739a28bc6d1b3bd8b96f818c457b575
somoire <rayr30667@gmail.com>
2019-05-25 11:20:12 -0400

    Refactoring the models

--- awards/models.py
+++ awards/models.py
@@ -47,3 +47,19 @@ class colors(models.Model):
     def delete_color(cls,colors):
         cls.objects.filter(colors = colors).delete()
 
+class countries(models.Model):
+    countries = models.CharField(max_length = 150)
+
+    def __str__(self):
+        return self.countries
+
+    class Meta:
+        ordering = ['countries']
+
+    def save_country(self):
+        self.save()
+
+    @classmethod
+    def delete_country(cls,countries):
+        cls.objects.filter(countries = countries).delete()
+
