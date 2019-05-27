11c392fb66621eadc4085afad61c45d8063d9137
somoire <rayr30667@gmail.com>
2019-05-27 08:52:59 -0400

    deploying

--- project/settings.py
+++ project/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES = ['default'].update(prod_db)
+DATABASES = { 'default': dj_database_url.config() } 
 
 
 # Password validation
